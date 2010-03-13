2ca687c53a555986b10607b67f8b81c131b58cc8
xorik <xorik@notebook>
2010-03-14 00:13:38 +0300

    css для админки

--- index.php
+++ index.php
@@ -8,11 +8,17 @@
 	// Кодировка и заголовок страницы
 	$HEAD[] = "<meta http-equiv='content-type' content='text/html; charset=utf-8'>";
 	$HEAD[] = "<title>{$config["title"]}</title>";
+	$CSS[] = "admin.css";
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <head>
 	<? head() ?>
 </head>
 <body>
-	
+	<div id='top'>
+		<a href='?logout=1'>Выйти</a>
+	</div>
+	<div id='content'>
+		
+	</div>
 </body>
