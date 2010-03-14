1dc33ec9eefe3440a83fc6c66091b7947400e53a
xorik <xorik@notebook>
2010-03-14 23:13:01 +0300

    Удаление страниц

--- modules/admin_sub.php
+++ modules/admin_sub.php
@@ -18,6 +18,33 @@
 		die;
 	}
 	
+	// Рекурсивное удаление страницы
+	function page_del( $id )
+	{
+		// Потомки
+		$query = "SELECT id FROM page WHERE gid=$id";
+		$res = mysql_query( $query );
+		while( $row = mysql_fetch_array($res) )
+			page_del( $row["id"] );
+		
+		// Сама страница
+		$query = "DELETE FROM page WHERE id=$id";
+		mysql_query( $query );
+	}
+	
+	// Удаление страниц
+	if( isset($_GET["page_del"]) )
+	{
+		// Удаление
+		$del = (int)$_GET["page_del"];
+		page_del( $del );
+		
+		// Переход обратно
+		$id = (int)$_GET["id"];
+		header( "Location: ?id=$id" );
+		die;
+	}
+	
 	function sub_content()
 	{
 		global $id;
@@ -36,7 +63,7 @@
 		while( $row = mysql_fetch_array($res) )
 		{
 			echo "<tr><td><a href='?id={$row["id"]}'><img src='modules/img/edit.png'> {$row["title"]}</a></td>";
-			echo "<td><a href='?id=$id&page_del={$row["id"]}'><img src='modules/img/del.png'> Удалить</a></td></tr>\n";
+			echo "<td><a href='?id=$id&page_del={$row["id"]}' onclick='if(confirm(\"Удалить {$row["title"]} вместе с подразделами?\")) return true; return false;'><img src='modules/img/del.png'> Удалить</a></td></tr>\n";
 		}
 		?>
 			<tr><td colspan='2'><a href='?page_add=<?= $id ?>'><img src='modules/img/add.png'> Добавить страницу</a></td></tr>
