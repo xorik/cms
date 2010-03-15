0680ea2751f3d4902aed21cf9c47fb7de5e432ac
xorik <xorik@notebook>
2010-03-15 17:09:51 +0300

    Хуки и инициализация в редактировании

--- modules/admin_base.php
+++ modules/admin_base.php
@@ -3,44 +3,76 @@
 	if( $_GET["do"] && $_GET["do"] != "edit" )
 		return;
 	
-	global $id;
-	global $gid;
+	hook_add( "base_show", "base_title", 10 );
+	hook_add( "base_show", "base_text", 90 );
+	hook_add( "init", "base_init", 10 );
 	
-	$id = (int)$_GET["id"];
-	
-	// Проверка есть ли страница
-	$query = "SELECT gid FROM page WHERE id=$id";
-	$res = mysql_query( $query );
-	// Есть страница
-	if( mysql_num_rows($res) )
+	function base_init()
 	{
-		$row = mysql_fetch_array( $res );
-		$gid = $row["gid"];
-		hook_add( "content", "base_content" );
+		global $id;
+		global $gid;
 		
-		// Обновление данных
-		if( $_POST )
+		$id = (int)$_GET["id"];
+		
+		// Проверка есть ли страница
+		$query = "SELECT gid FROM page WHERE id=$id";
+		$res = mysql_query( $query );
+		// Есть страница
+		if( mysql_num_rows($res) )
 		{
-			$query = "UPDATE page set title='{$_POST["title"]}', text='{$_POST["text"]}' WHERE id=$id";
-			mysql_query( $query );
-			clear_post();
+			$row = mysql_fetch_array( $res );
+			$gid = $row["gid"];
+			hook_add( "content", "base_content", 10 );
+			
+			// Обновление данных
+			if( $_POST )
+			{
+				$query = "UPDATE page set title='{$_POST["title"]}', text='{$_POST["text"]}' WHERE id=$id";
+				mysql_query( $query );
+				
+				// Обновление
+				hook_run( "base_submit", $id );
+				
+				clear_post();
+			}
 		}
 	}
 	
+	// Редактирование
 	function base_content()
 	{
 		global $id;
 		
-		$query = "SELECT title, text FROM page WHERE id=$id";
+		$query = "SELECT title FROM page WHERE id=$id";
 		$row = mysql_fetch_array( mysql_query($query) );
 		?>
 			<h3 id='base_toggle'><?= $row["title"] ?></h3>
 			<form method='post'>
-				Заголовок: <input type='text' name='title' value='<?= $row["title"] ?>'><br>
-				Текст:<br>
-				<textarea name='text' cols='80' rows='20'><?= $row["text"] ?></textarea><br>
+				<? hook_run( "base_show", $id ) ?>
 				<input type='submit' value='Сохранить'>
 			</form>
 		<?
 	}
+	
+	// Заголовок в редактировании
+	function base_title( $id )
+	{
+		$query = "SELECT title FROM page WHERE id=$id";
+		$row = mysql_fetch_array( mysql_query($query) );
+		?>
+			Заголовок: <input type='text' name='title' value='<?= $row["title"] ?>'><br>
+		<?
+	}
+	
+	// Текст в редактировании
+	function base_text( $id )
+	{
+		$query = "SELECT text FROM page WHERE id=$id";
+		$row = mysql_fetch_array( mysql_query($query) );
+		?>
+			Текст:<br>
+			<textarea name='text' cols='80' rows='20'><?= $row["text"] ?></textarea><br>
+		<?
+	}
+	
 ?>
