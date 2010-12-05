72161049d84031d1296e781504a1311250bd1c91
xorik <xorik@notebook>
2010-12-06 01:01:14 +0300

    Фикс .gitignore + забытый файл

---
+++ modules/files/tinymce.php
@@ -0,0 +1,25 @@
+<?
+	// Удаление дефолтной картинки
+	unhook( "files_show", "default_files_show" );
+	
+	hook( "files_show", "tinymce_files_show" );
+	
+	// При нажатии на картинку, она вставляется в редактор
+	function tinymce_files_show( $f )
+	{
+		if( $f["type"]=="png" || $f["type"]=="jpg" || $f["type"]=="jpeg" || $f["type"]=="gif" ) :
+		?>
+			<a href='javascript:void(0);' onclick='tinyMCE.execCommand( "mceInsertContent", false, "<img src=\"files/<?= $f["id"] ?>.<?= $f["type"] ?>\">");'>
+				<img src='files/<?= $f["id"] ?>_.jpg' class='pic'>
+			</a>
+		<?
+		else :
+		?>
+			<a href='javascript:void(0);' onclick='tinyMCE.execCommand( "mceInsertContent", false, "<a href=\"files/<?= $f["id"] ?>.<?= $f["type"] ?>\"><?= $f["filename"] ?></a>");'>
+				<img src='modules/img/file.png'> <?= $f["filename"] ?>
+			</a>
+		<?
+		endif;
+	}
+	
+?>
