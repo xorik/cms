d5cacc6c82668b41b3ea721434c1009a9fbc739b
xorik <xorik@notebook>
2012-02-05 14:25:38 +0400

    Удален модуль для подразделов, его картинки и код сортировки

--- admin.js
+++ admin.js
@@ -1,35 +1,5 @@
 $(function()
 {
-	// Сортировка разделов
-	$("#sub tbody").sortable(
-	{
-		items: "tr[id]",
-		opacity: 0.6,
-		containment: "#sub",
-		tolerance: "pointer",
-		stop: function()
-		{
-			// Вставить кнопку, если нужно
-			if( $("#sub a.save").size() == 0 )
-				$("#sub tr:last td").append("<a href='#' class='save'>Сохранить порядок</a>");
-			
-			// Новый порядок
-			var list = "";
-			$("#sub tr:[id]").each(function()
-			{
-				list += "&p[]="+$(this).attr("id");
-			});
-			
-			// Ссылка на страницу
-			var loc = document.location.href;
-			if( loc.indexOf("?") == -1 )
-				loc += "?id=0"
-			
-			// Сохранение сортировки
-			$("#sub a.save").attr("href", loc+"&page_sort=1"+list);
-		}
-	});
-	
 	// Сортировка файлов
 	$("#gallery").sortable(
 	{
