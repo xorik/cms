34c0576b5f854db249450ee01223b7c26bddef4d
xorik <xorik@notebook>
2012-02-08 11:44:41 +0400

    Вложенность уровней в блоке навигации

--- modules/admin/nav.php
+++ modules/admin/nav.php
@@ -1,6 +1,54 @@
 <?
 	hook( "nav", "nav_content" );
 	
+	// Отобразить текущий уровень
+	function nav_level( $nid, $level )
+	{
+		global $id;
+		global $GID;
+		global $LEVEL;
+		global $PAGE_TYPE;
+		
+		$query = "SELECT id, title, type, hide FROM page WHERE gid=$nid";
+		$res = mysql_query( $query );
+		while( $row = mysql_fetch_array($res) )
+		{
+			// Выделение блока
+			if( $row["id"] == $id )
+				$class = "sel";
+			elseif( $row["id"] == $GID[$level] )
+				$class = "open";
+			elseif( $level-1 == $LEVEL )
+				$class = "last";
+			else
+				$class = "";
+			// Скрытый ли блок
+			$show = $row["hide"] ? "hide" : "show";
+			echo "<li class='$class'>";
+				echo "<div class='sort'></div>";
+				// Отступы
+				for( $i=0; $i<$level-1; $i++ )
+					echo "<div class='indent'></div>";
+				echo "<a href='#' class='round del'></a>";
+				echo "<div class='round $show'></div>";
+				echo "<a href='".ADMIN."id={$row["id"]}' class='block'>{$row["title"]}";
+					// Стрелочка
+					if( !$PAGE_TYPE[$row["type"]]["nosub"] )
+						echo "<div class='arrow'></div>";
+				echo "</a>";
+			echo "</li>\n";
+			
+			// Подразделы
+			if( $row["id"] == $GID[$level] )
+			{
+				if( $level == $LEVEL )
+					echo "<div class='add'>Добавить подраздел <a href='#'></a></div>";
+				nav_level( $row["id"], $level+1 );
+				if( $level == $LEVEL )
+					echo "<hr>";
+			}
+		}
+	}
 	
 	// Отображение навигации
 	function nav_content()
@@ -12,29 +60,7 @@
 			</div>
 		<?
 		
-		global $id;
-		global $TYPE;
-		global $PAGE_TYPE;
-		
 		// Список первого уровня
-		$query = "SELECT id, title, type, hide FROM page WHERE gid=0";
-		$res = mysql_query( $query );
-		while( $row = mysql_fetch_array($res) )
-		{
-			// Выделение блока
-			$class =  $row["id"]==$id ? "class='sel'" : "";
-			// Скрытый ли блок
-			$show = $row["hide"] ? "hide" : "show";
-			echo "<li $class>";
-				echo "<div class='sort'></div>";
-				echo "<a href='#' class='round del'></a>";
-				echo "<div class='round $show'></div>";
-				echo "<a href='".ADMIN."id={$row["id"]}' class='block'>{$row["title"]}";
-					// Стрелочка
-					if( !$PAGE_TYPE[$row["type"]]["nosub"] )
-						echo "<div class='arrow'></div>";
-				echo "</a>";
-			echo "</li>\n";
-		}
+		nav_level( 0, 1 );
 	}
 ?>
