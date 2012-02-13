0b42596117c542979591c9aef06a96f777900c2e
xorik <xorik@notebook>
2012-02-13 16:27:31 +0400

    Переработан внешний вид настроек (теперь в таблицах и поля ввода как в остальной админке)

--- modules/config/base.php
+++ modules/config/base.php
@@ -63,29 +63,36 @@
 		global $CONFIG;
 		
 		?>
-			<h3>Настройки сайта</h3>
+			<h2>Настройки сайта</h2>
 			<form method='post'>
-			
-			Заголовок сайта: <input type='text' name='title' value='<?= $CONFIG["title"] ?>'><br>
-			Расширенные настройки (mysql, SEO): <input type='checkbox' name='adv' <? if( $CONFIG["adv"] ) echo "checked" ?>><br>
+			<table class='base'>
+				<col width='150'>
+				<col>
+			<tr><td>Заголовок сайта:</td> <td><input type='text' name='title' value='<?= $CONFIG["title"] ?>'></td></tr>
+			<tr><td>Расширенные настройки (mysql, SEO):</td> <td><input type='checkbox' name='adv' <? if( $CONFIG["adv"] ) echo "checked" ?>></td></tr>
 		<?
 			// Расширенные настройки
 			if( $CONFIG["adv"] ):
 			?>
-				Использовать mod_rewrite: <input type='checkbox' name='rewrite' <? if( $CONFIG["rewrite"] ) echo "checked" ?>><br>
-				id главной страницы: <input type='text' name='main' value='<?= $CONFIG["main"] ?>' size='2'><br>
+				<tr><td>Использовать mod_rewrite:</td> <td><input type='checkbox' name='rewrite' <? if( $CONFIG["rewrite"] ) echo "checked" ?>></td></tr>
+				<tr><td>id главной страницы:</td> <td><input type='text' name='main' value='<?= $CONFIG["main"] ?>' class='small'></td></tr>
 			<?
 			endif;
 		?>
-			<input type='submit' value='Сохранить'>
+			<tr><td colspan='2'><input type='submit' value='Сохранить'></td></tr>
+			</table>
 			</form>
 			
 			<h3>Смена пароля</h3>
 			<form method='post'>
-				Старый пароль: <input type='password' name='oldpass'><br>
-				Новый пароль: <input type='password' name='pass1'><br>
-				Еще раз: <input type='password' name='pass2'><br>
-				<input type='submit' value='Сохранить'>
+			<table class='base'>
+				<col width='150'>
+				<col>
+				<tr><td>Старый пароль:</td> <td><input type='password' name='oldpass'></td></tr>
+				<tr><td>Новый пароль:</td> <td><input type='password' name='pass1'></td></tr>
+				<tr><td>Еще раз:</td> <td><input type='password' name='pass2'></td></tr>
+				<tr><td colspan='2'><input type='submit' value='Сохранить'></td></tr>
+			</table>
 			</form>
 		<?
 	}
@@ -99,12 +106,16 @@
 		?>
 			<h3>Настройки MySQL</h3>
 			<form method='post'>
-			
-			host: <input type='text' name='db_host' value='<?= $CONFIG["db_host"] ?>'><br>
-			user: <input type='text' name='db_user' value='<?= $CONFIG["db_user"] ?>'><br>
-			pass: <input type='text' name='db_pass' value='<?= $CONFIG["db_pass"] ?>'><br>
-			db: <input type='text' name='db_db' value='<?= $CONFIG["db_db"] ?>'><br>
-			<input type='submit' value='Сохранить'>
+			<table class='base'>
+				<col width='150'>
+				<col>
+				
+				<tr><td>host:</td> <td><input type='text' name='db_host' value='<?= $CONFIG["db_host"] ?>'></td></tr>
+				<tr><td>user:</td> <td><input type='text' name='db_user' value='<?= $CONFIG["db_user"] ?>'></td></tr>
+				<tr><td>pass:</td> <td><input type='text' name='db_pass' value='<?= $CONFIG["db_pass"] ?>'></td></tr>
+				<tr><td>db:</td> <td><input type='text' name='db_db' value='<?= $CONFIG["db_db"] ?>'></td></tr>
+				<tr><td colspan='2'><input type='submit' value='Сохранить'></td></tr>
+			</table>
 			</form>
 		<?
 	}
