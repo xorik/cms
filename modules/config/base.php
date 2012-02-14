b6d2ff0f8c990dabf63ea349bd6ca1788d374214
xorik <xorik@notebook>
2012-02-14 12:37:54 +0400

    Пароль в настройках БД password вместо text, не менять данные, если пароль не задан

--- modules/config/base.php
+++ modules/config/base.php
@@ -12,7 +12,7 @@
 		{
 			hook( "content", "db_config", 90 );
 			// Сохранение настроек
-			if( $_POST["db_host"] )
+			if( $_POST["db_pass"] )
 			{
 				foreach( array("db_host", "db_user", "db_pass", "db_db") AS $v )
 					$CONFIG[$v] = $_POST[$v];
@@ -112,7 +112,7 @@
 				
 				<tr><td>host:</td> <td><input type='text' name='db_host' value='<?= $CONFIG["db_host"] ?>'></td></tr>
 				<tr><td>user:</td> <td><input type='text' name='db_user' value='<?= $CONFIG["db_user"] ?>'></td></tr>
-				<tr><td>pass:</td> <td><input type='text' name='db_pass' value='<?= $CONFIG["db_pass"] ?>'></td></tr>
+				<tr><td>pass:</td> <td><input type='password' name='db_pass'></td></tr>
 				<tr><td>db:</td> <td><input type='text' name='db_db' value='<?= $CONFIG["db_db"] ?>'></td></tr>
 				<tr><td colspan='2'><input type='submit' value='Сохранить'></td></tr>
 			</table>
