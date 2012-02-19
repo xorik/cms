98af0210308c1be767eb4668cb2e82cb2cf3b99b
xorik <xorik@notebook>
2012-02-19 18:53:29 +0400

    Функции определения ревизии и коммита для старого гита (debian stable)

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 tag=`git tag | tail -n1`
-rev=`git log --oneline | wc -l`
-ci=`git log HEAD~..HEAD --oneline | cut --delimiter=' ' -f 1`
+rev=`git log | grep Author | wc -l`
+ci=`git log HEAD~..HEAD | head -n1 | cut --delimiter=' ' -f 2 | head -c 7`
 date=`git log HEAD~..HEAD --date=short | grep -o [0-9]*-[0-9]*-[0-9]*`
 
 echo "$tag (r$rev $ci $date)" > version
