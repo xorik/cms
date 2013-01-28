d37d208e5f93bb05d5180107e034d86260f2709d
xorik <xorik@notebook>
2013-01-28 13:47:18 +0400

    <!doctype html> в админке тоже

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
 	$JS[] = "jquery-ui.js";
 	$JS[] = "admin.js";
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!doctype html>
 <head>
 	<? head() ?>
 </head>
