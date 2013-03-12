ff16bdc32ab76a6093088121495122d6623ba367
xorik <xorik@notebook>
2013-03-12 16:36:46 +0400

    Переделана функция menu, починен баг с главной страницой в меню

--- modules/content/menu.php
+++ modules/content/menu.php
@@ -1,17 +1,24 @@
 <?
 	// Отобразить меню
-	function menu( $id, $templ="{}" )
+	function menu( $gid, $templ="{}" )
 	{
+		global $id;
 		global $GID;
+		global $CONFIG;
 		
-		$query = "SELECT id, title FROM page WHERE gid=$id AND hide=0 ORDER BY pos, id";
+		$query = "SELECT id, title FROM page WHERE gid=$gid AND hide=0 ORDER BY pos, id";
 		$res = mysql_query( $query );
 		while( $row = mysql_fetch_array($res) )
 		{
+			// Один из подразделов
+			$sel = "";
 			if( in_array($row["id"], $GID) )
-				$s = "<a href='".path($row["id"])."' class='sel'>{$row["title"]}</a>";
-			else
-				$s = "<a href='".path($row["id"])."'>{$row["title"]}</a>";
+			{
+				if( ($row["id"]==$CONFIG["main"] && $id==$row["id"]) || $row["id"]!=$CONFIG["main"] )
+					$sel = "class='sel'";
+			}
+			
+			$s = "<a href='".path($row["id"])."' $sel>{$row["title"]}</a>";
 			
 			echo str_replace( "{}", $s, $templ );
 		}
