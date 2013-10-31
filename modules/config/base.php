c6377bfbf68dc4ed8cea86f3ca0502c86b42df94
xorik <xor29a@bk.ru>
2013-10-31 11:37:58 +0400

    Загрузка файлов по ссылке в расширенные настройки

--- modules/config/base.php
+++ modules/config/base.php
@@ -63,12 +63,12 @@
 				<col>
 			<tr><td>Заголовок сайта:</td> <td><input type='text' name='title' value='<?= $CONFIG["title"] ?>'></td></tr>
 			<tr><td>Расширенные настройки (mysql, SEO):</td> <td><input type='checkbox' name='adv' <? if( $CONFIG["adv"] ) echo "checked" ?>></td></tr>
-			<tr><td>Загрузка файлов по ссылке:</td> <td><input type='checkbox' name='load_url' <? if( $CONFIG["load_url"] ) echo "checked" ?>></td></tr>
 		<?
 			// Расширенные настройки
 			if( $CONFIG["adv"] ):
 			?>
 				<tr><td>Использовать mod_rewrite:</td> <td><input type='checkbox' name='rewrite' <? if( $CONFIG["rewrite"] ) echo "checked" ?>></td></tr>
+				<tr><td>Загрузка файлов по ссылке:</td> <td><input type='checkbox' name='load_url' <? if( $CONFIG["load_url"] ) echo "checked" ?>></td></tr>
 				<tr><td>Основной шаблон:</td> <td><input type='text' name='template' value='<?= $CONFIG["template"] ?>'></td></tr>
 				<tr><td>id главной страницы:</td> <td><input type='text' name='main' value='<?= $CONFIG["main"] ?>' class='small'></td></tr>
 				<tr><td>id страницы 404:</td> <td><input type='text' name='404_page' value='<?= $CONFIG["404_page"] ?>' class='small'> <small>(Оставьте 0, чтобы использовать стандартную)</small></td></tr>
