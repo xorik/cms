7f6835b6cfc2fcb66876353e72f5bcd0f3a36311
xorik <xor29a@bk.ru>
2014-12-30 14:45:55 +0300

    Move modules to /modules/hooks

--- modules/hooks/files/img_insert.php
+++ modules/hooks/files/img_insert.php
@@ -19,7 +19,7 @@
 			if( $CONFIG["rewrite"] )
 				$text = "<a href='file/{$f["id"]}'>{$f["filename"]}</a>";
 			else
-				$text = "<a href='./?t=file/{$f["id"]}'>{$f["filename"]}</a>";
+				$text = "filename";
 			
 			$html = "<i class='i-file'></i> {$f["filename"]}";
 		}
