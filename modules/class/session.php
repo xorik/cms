649b5af290e271fff42642b62c1e10de39a75fd4
xorik <xor29a@bk.ru>
2015-01-02 00:52:13 +0300

    Session class

---
+++ modules/class/session.php
@@ -0,0 +1,77 @@
+<?php
+
+
+// TODO: custom timeout
+
+
+class Session
+{
+	const SESSION_COOKIE = "sess";
+	const SESSION_DIR = "cache/sess";
+	const CLEANUP_TIME = 86400;
+	const CLEANUP_CHANCE = 1000;
+
+	static protected $sess = array();
+	static protected $changed = false;
+
+
+	static public function init()
+	{
+		// Load session
+		if( isset($_COOKIE[self::SESSION_COOKIE]) && is_file($file=self::SESSION_DIR ."/sess-".$_COOKIE[self::SESSION_COOKIE]) )
+		{
+			self::$sess = json( file_get_contents($file) );
+		}
+		// Create new session
+		else
+		{
+			$key =  sha1( microtime(true) . rand() );
+			setcookie( self::SESSION_COOKIE, $key );
+			$_COOKIE[self::SESSION_COOKIE] = $key;
+			self::$changed = true;
+		}
+	}
+
+	static public function get( $key )
+	{
+		if( isset(self::$sess[$key]) )
+			return self::$sess[$key];
+		else
+			return false;
+	}
+
+	static public function set( $key, $value )
+	{
+		self::$sess[$key] = $value;
+		self::$changed = true;
+	}
+
+	static public function save()
+	{
+		if( self::$changed )
+		{
+			// Create directory
+			if( !is_dir(self::SESSION_DIR) )
+			{
+				$res = mkdir( self::SESSION_DIR, 0700 );
+				if( !$res )
+					throw new Exception( "Can't create directory ". self::SESSION_DIR );
+			}
+
+			$res = file_put_contents( self::SESSION_DIR ."/sess-".$_COOKIE[self::SESSION_COOKIE], json(self::$sess) );
+			if( !$res )
+				throw new Exception( "Can't save session" );
+		}
+
+		// Cleanup
+		if( rand(1, self::CLEANUP_CHANCE)==1 )
+		{
+			$mintime = time() - self::CLEANUP_TIME;
+			foreach( glob(self::SESSION_DIR ."/sess-*") as $file )
+			{
+				if( filemtime($file) < $mintime )
+					unlink( $file );
+			}
+		}
+	}
+}
