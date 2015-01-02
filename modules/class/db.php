362ad9b091faec9168d0f4a09e007c29d57bf8d3
xorik <xor29a@bk.ru>
2015-01-02 12:55:29 +0300

    DB class for MySQL, remove old functions

---
+++ modules/class/db.php
@@ -0,0 +1,185 @@
+<?php
+
+
+class DB
+{
+	static protected $db = null;
+
+
+	/**
+	 * Connect to MySQL
+	 *
+	 * @throws Exception on error
+	 */
+	static public function init()
+	{
+		$db = Config::get( "db" );
+		self::$db = new mysqli( $db["host"], $db["user"], $db["pass"], $db["db"] );
+		if( self::$db->connect_errno )
+			throw new Exception( "MySQL connect error" );
+
+		self::query( "SET NAMES UTF8" );
+	}
+
+	/**
+	 * Escape value for query
+	 *
+	 * @param mixed $var
+	 * @return string escaped string
+	 */
+	static public function escape( $var )
+	{
+		if( is_string($var) )
+		{
+			return "'". self::$db->escape_string($var) ."'";
+		}
+		elseif( is_numeric($var) )
+		{
+			return $var;
+		}
+		elseif( is_null($var) )
+			return "NULL";
+
+		trigger_error( "Unknown type for escaping: ". print_r($var, true) );
+		return "''";
+	}
+
+	/**
+	 * Run MySQL query
+	 *
+	 * @param string $query
+	 * @return mysqli_result|false
+	 */
+	static public function query( $query )
+	{
+		$res = self::$db->query( $query );
+
+		if( $res === false )
+		{
+			trigger_error( "Error running query '$query'<br>". self::$db->error );
+			return false;
+		}
+
+		return $res;
+	}
+
+	/**
+	 * Get all rows for query result
+	 *
+	 * @param string $query
+	 * @return array|false
+	 */
+	static public function all( $query )
+	{
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		$rows = array();
+		while( $row = $res->fetch_assoc() )
+			$rows[] = $row;
+
+		$res->free();
+		return $rows;
+	}
+
+	/**
+	 * Get one row from query result
+	 *
+	 * @param string $query
+	 * @return array|false
+	 */
+	static public function row( $query )
+	{
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		$tmp = $res->fetch_assoc();
+		$res->free();
+		return $tmp;
+	}
+
+	/**
+	 * Get one field from query result
+	 *
+	 * @param string $query
+	 * @return mixed|false
+	 */
+	static public function one( $query )
+	{
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		list($tmp) = $res->fetch_row();
+		$res->free();
+		return $tmp;
+	}
+
+	/**
+	 * Insert into DB
+	 *
+	 * @param string $table
+	 * @param array $fields
+	 * @param int $replace replace or inserts
+	 * @param int $unesc unescape field values
+	 * @return bool|int insert_id or false on error
+	 */
+	static public function insert( $table, $fields, $replace=0, $unesc=0 )
+	{
+		$a = $b = array();
+		foreach( $fields as $k=>$v )
+		{
+			$a[] = "`$k`";
+			$b[] = $unesc ? $v : self::escape( $v );
+		}
+
+		$cmd = $replace ? "REPLACE" : "INSERT";
+		$query = "$cmd INTO `$table` (". implode(", ", $a) .") VALUES(". implode(", ", $b) .")";
+
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		return self::$db->insert_id;
+	}
+
+	/**
+	 * Update table
+	 *
+	 * @param string $table
+	 * @param array $fields
+	 * @param string $where
+	 * @param int $unesc unescape field values
+	 * @return bool|integer affected_rows or false on error
+	 */
+	static public function update( $table, $fields, $where, $unesc=0 )
+	{
+		$a = array();
+		foreach( $fields as $k=>$v )
+		{
+			$a[] = "`$k`=". ($unesc ? $v : self::escape($v));
+		}
+
+		$query = "UPDATE `$table` SET ". implode(", ", $a) ." WHERE $where";
+
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		return self::$db->affected_rows;
+	}
+
+	/**
+	 * Delete row from table
+	 *
+	 * @param $table
+	 * @param $where
+	 * @return bool|integer affected_rows or false on error
+	 */
+	static public function delete( $table, $where )
+	{
+		$query = "DELETE FROM `$table` WHERE $where";
+
+		$res = self::query( $query );
+		if( !$res ) return false;
+
+		return self::$db->affected_rows;
+	}
+}
