b7d6227617e3b268962416b6a43f357241fa7388
xorik <xor29a@bk.ru>
2015-01-05 02:03:05 +0300

    Page::init(), to parse page id, title and type from path

--- modules/class/page.php
+++ modules/class/page.php
@@ -3,6 +3,43 @@
 
 class Page
 {
+	static public function init( $tmp, $reload )
+	{
+		// Get page id from $_GET[id] or main page
+		if( isset($_GET["id"]) || !Router::$path )
+		{
+			$id = isset($_GET["id"]) ? (int)$_GET["id"] : (int)Config::get("main");
+			$row = DB::row( "SELECT type, title FROM page WHERE id=$id" );
+			if( $row )
+			{
+				// Reload to path
+				if( $reload && $id!=Config::get("main") && $path=self::prop($id, "path") )
+				{
+					header( "Location: ". Router::$root . $path );
+					die;
+				}
+			}
+			else
+				$id = null;
+		}
+		// Get page id from path
+		else
+		{
+			$id = DB::one( "SELECT id FROM prop WHERE field='path' AND value=". DB::escape(Router::$path) );
+			if( $id )
+				$row = DB::row( "SELECT type, title FROM page WHERE id=$id" );
+			else
+				$id = null;
+		}
+
+		Heap::set( "id", $id );
+		if( !$id )
+			return;
+
+		Heap::set( "title", $row["title"] );
+		Heap::set( "type", $row["type"] );
+	}
+
 	static public function text( $id )
 	{
 		return DB::one( "SELECT text FROM page WHERE id=". DB::escape($id) );
--- modules/class/router.php
+++ modules/class/router.php
@@ -4,17 +4,21 @@
 class Router
 {
 	static public $root;
+	static public $path;
+
 
 	static public function init()
 	{
+		// Parse URL
 		$tmp = self::$root = str_replace( "index.php", "", $_SERVER["PHP_SELF"] );
 		$tmp = str_replace( $tmp, "", $_SERVER["REQUEST_URI"] );
 		$tmp = explode( "?", $tmp );
+		self::$path = $tmp[0];
 
 		// Route hooks
 		Hook::add( "route", "Router::default_route", 999 );
 		Module::load( "route" );
-		Hook::run( "route", $tmp[0] );
+		Hook::run( "route", urldecode(self::$path) );
 	}
 	
 	static public function default_route( $path )
