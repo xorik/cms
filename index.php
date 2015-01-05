034aa0ddb0474bf196c749ecc33f01c1655bc2d3
xorik <xor29a@bk.ru>
2015-01-05 12:03:42 +0300

    New index.php and default router

--- index.php
+++ index.php
@@ -1,134 +1,27 @@
 <?php
-	require( "config.php" );
-	require( "modules/func.php" );
-	require( "modules/template.php" );
-	
-	header( "Content-type: text/html; charset=utf-8" );
-	
-	$BASEPATH = str_replace("index.php", "", $_SERVER["SCRIPT_NAME"]);
-	$BASEURL = "http". ($_SERVER["HTTPS"]?"s":"") ."://". $_SERVER["HTTP_HOST"] . $BASEPATH;
-	
-	load_modules( "all" );
-	
-	// id страницы
-	// Админка или настройки
-	if( ($_GET["t"]=="admin" || $_GET["t"]=="config") && $_GET["do"]!="ajax" )
-		$_GET["do"] = $_GET["t"];
-	
-	// Файл
-	elseif( strpos($_GET["t"], "file/") === 0 )
-	{
-		preg_match( "|file/(.*)|", $_GET["t"], $m );
-		$_GET = array( "do"=>"ajax", "file"=>"getfile", "fid"=>$m[1] );
-	}
-	// Страница или ajax
-	else
-	{
-		if( $_GET["t"] && $_GET["do"]!="ajax" )
-		{
-			$row = db_select_one( "SELECT id FROM prop WHERE field='path' AND value=". db_escape($_GET["t"]) );
-			$id = $row["id"];
-		}
-		// id 0 для админки
-		elseif($_GET["do"]=="ajax" && $_GET["file"]=="admin" && !$_GET["id"])
-			$id = 0;
-		// Неправильный путь, страница по id или главная
-		elseif( $_GET["do"]!="ajax" || $_GET["id"] )
-			$id = $_GET["id"] ? (int)$_GET["id"] : $CONFIG["main"];
-	}
-	
-	// Переход с ?id= на путь страницы, если включен реврайт
-	if( $_GET["id"] && $id && $id!=$CONFIG["main"] && !$_GET["do"] && $CONFIG["rewrite"] )
-	{
-		if( $path = get_prop($id, "path") )
-		{
-			header( "Location: $path" );
-			die;
-		}
-	}
-	
-	// Страница выбрана
-	if( $id )
-	{
-		// Заголовок и тип
-		$row = db_select_one( "SELECT title, type FROM page WHERE id=$id" );
-		// Страница существует
-		if( $row )
-		{
-			$ORIG_TITLE = $row["title"];
-			$TITLE = "{$CONFIG["title"]} - $ORIG_TITLE";
-			$TYPE = $row["type"];
-		}
-		else
-			unset( $id );
-	}
-	
-	// Вернуть статус 404, если страницы нет
-	if( !$id && !$_GET["do"] )
-		header( "HTTP/1.0 404 Not Found" );
-	
-	// Начать PHP-сессию
-	session_start();
-	
-	// Инициализация
-	$ADMIN_URL = $CONFIG["rewrite"] ? "./admin?" : "./?t=admin&";
-	$CONFIG_URL = $CONFIG["rewrite"] ? "./config?" : "./?t=config&";
-	
-	// Фикс для шаблонов
-	$HEAD = $CSS = $JS = $SCRIPT = array();
-	
-	// Действие
-	$DO = $_GET["do"];
-	// Админка
-	if( $DO=="admin" || $DO=="config" )
-	{
-		// Проверка прав
-		run( "auth" );
-		load_modules( $DO );
-		run( "init" );
-		
-		// Заголовок
-		$HEAD[] = "<title>Страница администратора - {$CONFIG["title"]}</title>";
-		
-		template( "modules/templates/admin.tpl" );
-	}
-	// Аяксовая функция
-	elseif( $DO=="ajax" )
-	{
-		// Проверка на запрещенные символы
-		if( strpos($_GET["file"], "*")!==false || strpos($_GET["file"], "..")!==false )
-			$file = "";
-		// Файл в modules
-		elseif( is_file("modules/ajax/{$_GET["file"]}.php") )
-			$file = "modules/ajax/{$_GET["file"]}.php";
-		// Файл в extra
-		else
-		{
-			$files = glob( "extra/*/ajax/{$_GET["file"]}.php" );
-			$file = $files[0];
-		}
-		
-		if( $file )
-			@include( $file );
-		else
-			header( "HTTP/1.0 404 Not Found" );
-		
-		die();
-	}
-	// Контент
-	else
-	{
-		// Инициализация
-		load_modules( "content" );
-		run( "init" );
-		
-		// Заголовок страницы
-		$HEAD[] = "<title>$TITLE</title>";
-		// Шаблон контента
-		if( substr_compare($CONFIG["template"], ".tpl", -4, 4) === 0 )
-			template( $CONFIG["template"], 1 );
-		else
-			require( $CONFIG["template"] );
-	}
-	
-	run( "shutdown" );
+
+
+header( "Content-type: text/html; charset=utf-8" );
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+
+// Load and init basic modules
+require( "modules/func.php" );
+require( "modules/class/localcache.php" );
+LocalCache::init();
+Config::init();
+
+if( Config::get("cache") === CACHE_LEVEL_OFF )
+	LocalCache::scan();
+
+// Connect to database
+Hook::add( "init", "DB::init", 50 );
+
+// Load session, if session cookie is set
+if( isset($_COOKIE[Session::SESSION_COOKIE]) )
+	Hook::add( "init", "Session::init", 100 );
+
+Module::load( "all" );
+Router::init();
+
+Hook::run( "shutdown" );
--- modules/class/router.php
+++ modules/class/router.php
@@ -23,6 +23,68 @@ class Router
 	
 	static public function default_route( $path )
 	{
-		
+		if( isset($_GET["id"]) )
+			Hook::add( "init", "Page::init", 60, 0 );
+
+		// Ajax handler
+		if( preg_match("|ajax/(.+)|", $path, $m) )
+		{
+			if( !isset(LocalCache::$ajax[$m[1]]) || !is_file($file=LocalCache::$ajax[$m[1]]) )
+			{
+				header( "HTTP/1.0 404 Not Found" );
+				echo "Ajax handler for request '{$m[1]}' is not found";
+
+				return;
+			}
+
+			Hook::run( "init" );
+			require( $file );
+
+			return;
+		}
+		// Get file
+		elseif( preg_match("|file/(.+)|", $path, $m) )
+		{
+			Hook::run( "init" );
+			File::download( $m[1] );
+
+			return;
+		}
+
+		Head::$head[] = "<meta charset='utf-8'>";
+
+		// Admin or config site
+		if( $path == "admin" || $path == "config" )
+		{
+			Hook::add( "init", "Session::init", 100 );
+			Module::load( $path );
+			Module::load( "auth" );
+			Hook::run( "init" );
+
+			$tpl = "modules/templates/admin.tpl";
+		}
+		// Content
+		else
+		{
+			if( !isset($_GET["id"]) )
+				Hook::add( "init", "Page::init", 60, 1 );
+
+			Module::load( "content" );
+			Hook::run( "init" );
+
+			$tpl = Config::get("template");
+			if( !$tpl )
+				$tpl = "templates/main.tpl";
+		}
+
+		// Content template
+		$content = Template::get( $tpl );
+		// TODO: save content for hook
+
+		echo Head::get();
+		echo $content;
+
+		if( Head::$new_js )
+			echo Head::scripts();
 	}
 }
