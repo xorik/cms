217338f60d31151212ed234c7aaf8c4d7b378af6
xorik <xor29a@bk.ru>
2015-01-08 00:12:21 +0300

    Simple classes for page types

---
+++ modules/class/type.php
@@ -0,0 +1,50 @@
+<?php
+
+// Root and simple page type
+define( "DEFAULT_PAGE_TYPE", "Страница" );
+Types::set( DEFAULT_PAGE_TYPE, "Обычная страница с текстом и картинками" );
+Types::set( "root", "", array(DEFAULT_PAGE_TYPE) );
+
+
+class Type
+{
+	public $descr;
+	public $sub = array();
+	public $editor;
+	public $files;
+	public $removable;
+	public $lock_type;
+	public $virt;
+	public $reverse;
+}
+
+
+class Types {
+	static public $types = array();
+
+
+	static public function set( $title, $descr="", $sub=array(), $editor=1, $files=1, $removable=1, $lock_type=0, $virt=0, $reverse=0 )
+	{
+		$type = new Type;
+		$type->descr = $descr;
+		$type->sub = $sub;
+		$type->editor = $editor;
+		$type->files = $files;
+		$type->removable = $removable;
+		$type->lock_type = $lock_type;
+		$type->virt = $virt;
+		$type->reverse = $reverse;
+
+		self::$types[$title] = $type;
+	}
+
+	static public function get( $title )
+	{
+		if( isset(self::$types[$title]) )
+			return self::$types[$title];
+		elseif( isset(self::$types[DEFAULT_PAGE_TYPE]) )
+			return self::$types[DEFAULT_PAGE_TYPE];
+		else
+			return false;
+	}
+}
