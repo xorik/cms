232d02962af0c530cfa94e92cd27230e34ceb8ce
xorik <xor29a@bk.ru>
2015-01-08 18:50:51 +0300

    Fix admin nav ajax script

--- modules/ajax/nav.php
+++ modules/ajax/nav.php
@@ -1,69 +1,64 @@
 <?php
-	run( "auth" );
-	
-	load_modules( "admin" );
-	run( "init" );
-	
-	// Отобразить текущий уровень
-	function nav_level( $nid, $level, $type )
-	{
-		global $PAGE_TYPE;
-		
-		// Прямая или обратная сортировка
-		if( $PAGE_TYPE[$type]["reverse"] )
-			$order = "pos DESC, id DESC";
-		else
-			$order = "pos, id";
 
-		$rows = db_select( "SELECT id, title, type, hide FROM page WHERE gid=$nid ORDER BY $order" );
-		foreach( $rows as $row )
+Hook::add( "init", "Auth::init", 200 );
+Hook::run( "init" );
+
+// Show current level
+function nav_level( $nid, $level, $type )
+{
+	// Sorting order
+	if( Types::get($type)->reverse )
+		$order = "pos DESC, id DESC";
+	else
+		$order = "pos, id";
+
+	$rows = DB::all( "SELECT id, title, type, hide FROM page WHERE gid=$nid ORDER BY $order" );
+	foreach( $rows as $row )
+	{
+		// Hidden page
+		$show = $row["hide"] ? "hide" : "show";
+		echo "<li><i class='i-sort'></i>";
+		// Ident
+		for( $i=0; $i<$level-1; $i++ )
 		{
-			// Скрытый ли блок
-			$show = $row["hide"] ? "hide" : "show";
-			echo "<li>";
-				echo "<i class='i-sort'></i>";
-				// Отступы
-				for( $i=0; $i<$level-1; $i++ )
-				{
-					echo "<div class='indent'></div>";
-				}
-				// Не неудаляемая
-				if( !$PAGE_TYPE[$row["type"]]["lock"] )
-				{
-					echo "<a href='#' class='round del' data-title='Удалить \"{$row["title"]}\" вместе с подразделами?'><i class='i-del'></i></a>";
-				}
-				// Неудаляемая
-				else
-				{
-					echo "<div class='round lock'></div>";
-				}
-				echo "<div class='round $show'><i class='i-'></i></div>";
-				echo "<a href='?id={$row["id"]}' class='block' data-id='{$row["id"]}' title='{$row["title"]}'>{$row["title"]}";
-					// Стрелочка
-					if( !empty($PAGE_TYPE[$row["type"]]["sub"]) )
-					{
-						echo "<i class='i-arrow'></i>";
-					}
-				echo "</a>";
-			echo "</li>\n";
-			
-			// Подразделы
-			if( !empty($PAGE_TYPE[$row["type"]]["sub"]) )
-			{
-				echo "<div class='sub'>";
-				// Кнопка "добавить подраздел"
-				$type = $PAGE_TYPE[$row["type"]]["sub"][0] ? $PAGE_TYPE[$row["type"]]["sub"][0] : "Страница";
-				echo "<div class='add'>Добавить подраздел <a href='#' data-gid='{$row["id"]}' data-type='$type'><i class='i-plus'></i></a></div>";
-				nav_level( $row["id"], $level+1, $row["type"] );
-				echo "<hr></div>";
-			}
+			echo "<div class='indent'></div>";
+		}
+		// Removable
+		if( Types::get($row["type"])->removable )
+		{
+			echo "<a href='#' class='round del' data-title='Удалить \"{$row["title"]}\" вместе с подразделами?'><i class='i-del'></i></a>";
+		}
+		// Non-removable
+		else
+		{
+			echo "<div class='round lock'></div>";
+		}
+		echo "<div class='round $show'><i class='i-'></i></div>";
+		echo "<a href='?id={$row["id"]}' class='block' data-id='{$row["id"]}' title='{$row["title"]}'>{$row["title"]}";
+		// Arrow
+		if( !empty($PAGE_TYPE[$row["type"]]["sub"]) )
+			echo "<i class='i-arrow'></i>";
+
+		echo "</a></li>";
+
+		// Sub-pages
+		if( !empty(Types::get($row["type"])->sub) )
+		{
+			echo "<div class='sub'>";
+			// Add button
+			$type = Types::get($row["type"])->sub[0];
+			echo "<div class='add'>Добавить подраздел <a href='#' data-gid='{$row["id"]}' data-type='$type'><i class='i-plus'></i></a></div>";
+			nav_level( $row["id"], $level+1, $row["type"] );
+			echo "<hr></div>";
 		}
 	}
-	
-	echo "<div id='nav_title'>";
-	$type = $PAGE_TYPE["root"]["sub"][0] ? $PAGE_TYPE["root"]["sub"][0] : "Страница";
-	echo "<a href='#' class='add' data-gid='0' data-type='$type'><i class='i-plus'></i></a>";
-	echo "<a href='{$ADMIN_URL}' data-id='0'>Разделы</a></div>\n";
-	
-	// Список первого уровня
-	nav_level( 0, 1, "" );
+}
+
+echo "<div id='nav_title'>";
+$type = Types::get("root")->sub[0] ? Types::get("root")->sub[0] : DEFAULT_PAGE_TYPE;
+echo "<a href='#' class='add' data-gid='0' data-type='$type'><i class='i-plus'></i></a>";
+echo "<a href='". Router::$root ."admin' data-id='0'>Разделы</a></div>";
+
+// Root list
+nav_level( 0, 1, "" );
+
