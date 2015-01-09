949180ff24fccf666ed6b38be31afd2b5b6d1de6
xorik <xor29a@bk.ru>
2015-01-09 15:25:15 +0300

    Page functions for get level, type, gid or title of any id and level + caching

--- modules/class/page.php
+++ modules/class/page.php
@@ -3,13 +3,17 @@
 
 class Page
 {
+	static protected $id = null;
+	static protected $cache = null;
+
+
 	static public function init( $tmp, $reload )
 	{
 		// Get page id from $_GET[id] or main page
 		if( isset($_GET["id"]) || !Router::$path )
 		{
 			$id = isset($_GET["id"]) ? (int)$_GET["id"] : (int)Config::get("main");
-			$row = DB::row( "SELECT type, title FROM page WHERE id=$id" );
+			$row = self::get( $id );
 			if( $row )
 			{
 				// Reload to path
@@ -27,17 +31,31 @@ class Page
 		{
 			$id = DB::one( "SELECT id FROM prop WHERE field='path' AND value=". DB::escape(Router::$path) );
 			if( $id )
-				$row = DB::row( "SELECT type, title FROM page WHERE id=$id" );
+				$row = self::get( $id );
 			else
 				$id = null;
 		}
 
+		self::$id = $id;
 		Heap::set( "id", $id );
 		if( !$id )
 			return;
 
 		Heap::set( "title", $row["title"] );
 		Heap::set( "type", $row["type"] );
+	}
+
+	static public function get( $id )
+	{
+		if( isset(self::$cache[$id]["gid"]) )
+			return self::$cache[$id];
+
+		$res = DB::row( "SELECT gid, type, title FROM page WHERE id=". DB::escape($id) );
+
+		if( $res )
+			self::$cache[$id] = $res;
+
+		return $res;
 	}
 
 	static public function text( $id )
@@ -66,5 +84,96 @@ class Page
 			DB::insert( "prop", array("id"=>$id, "field"=>$key, "value"=>$value), 1 );
 		else
 			DB::delete( "prop", $where );
+	}
+
+	static public function gid( $level=null, $id=null )
+	{
+		$id = $id ? (int)$id : self::$id;
+		if( !$level )
+		{
+			$row = self::get( $id );
+			return $row ? $row["gid"] : false;
+		}
+
+		$level = $level - 1;
+		if( !isset(self::$cache[$id]["gids"][$level]) && !self::level($id) )
+			return false;
+
+		return self::$cache[$id]["gids"][$level];
+	}
+
+	static public function title( $level, $id=null )
+	{
+		$id = $id ? (int)$id : self::$id;
+		if( !$level )
+		{
+			$row = self::get( $id );
+			return $row ? $row["title"] : false;
+		}
+
+		$level = $level - 1;
+		if( !isset(self::$cache[$id]["titles"][$level]) && !self::level($id) )
+			return false;
+
+		return self::$cache[$id]["titles"][$level];
+	}
+
+	static public function type( $level, $id=null )
+	{
+		$id = $id ? (int)$id : self::$id;
+		if( !$level )
+		{
+			$row = self::get( $id );
+			return $row ? $row["type"] : false;
+		}
+
+		$level = $level - 1;
+		if( !isset(self::$cache[$id]["types"][$level]) && !self::level($id) )
+			return false;
+
+		return self::$cache[$id]["types"][$level];
+	}
+
+
+	static public function level( $id=null )
+	{
+		$id = $id ? (int)$id : self::$id;
+
+		// Check cache
+		if( isset(self::$cache[$id]["level"]) )
+			return self::$cache[$id]["level"];
+
+		// Calculate level + fill gid, title and type cache
+		$row = self::get($id);
+		if( !$row )
+			return false;
+
+		$gid = (int)$row["gid"];
+
+		// Parent is root
+		if( $gid == 0 )
+		{
+			self::$cache[$id]["level"] = 1;
+			self::$cache[$id]["gids"] = array( $gid );
+			self::$cache[$id]["titles"] = array( $row["title"] );
+			self::$cache[$id]["types"] = array( $row["type"] );
+
+			return 1;
+		}
+		// Get parent from cache, add current level
+		elseif( isset(self::$cache[$gid]["level"]) || self::level($gid) )
+		{
+			self::$cache[$id]["level"] = self::$cache[$gid]["level"] + 1;
+			self::$cache[$id]["gids"] = self::$cache[$gid]["gids"];
+			self::$cache[$id]["gids"][] = $gid;
+			self::$cache[$id]["titles"] = self::$cache[$gid]["titles"];
+			self::$cache[$id]["titles"][] = $row["title"];
+			self::$cache[$id]["types"] = self::$cache[$gid]["types"];
+			self::$cache[$id]["types"][] = $row["title"];
+
+			return self::$cache[$id]["level"];
+		}
+		else
+			return false;
 	}
 }
