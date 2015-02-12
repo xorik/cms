10bccd7f7786c04e5e0ac674491dd42bc540af34
xorik <xor29a@bk.ru>
2015-02-12 22:58:31 +0300

    Support for max image size, enable scroller by default and set max img size 2000x2000

--- modules/ajax/upload.php
+++ modules/ajax/upload.php
@@ -57,8 +57,18 @@ function gallery_upload( $f )
 
 	if( $type == "png" )
 		Img::$bg_fill = array( 245, 245, 245, 0 );
-	else
-		Img::$bg_fill = false;
 
 	Img::resize( $f["path"], 300, 64, RESIZE_METHOD_MAX_SIDE, File::path($f["id"], "", "jpg") );
+
+	// Max image size
+	$max = Config::get( "files", "max_img" );
+	$size = Img::size( $f["path"] );
+	if( isset($max[0]) && isset($max[1]) && ($size[0]>$max[0] || $size[1]>$max[1]) )
+	{
+		if( $type == "png" )
+			Img::$bg_fill = null;
+
+		Img::resize( $f["path"], $max[0], $max[1], RESIZE_METHOD_MAX_SIDE, false, $type );
+	}
+	Img::$bg_fill = false;
 }
