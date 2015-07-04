bdc8f15c56768980b00e19f8507677072488d038
xorik <xor29a@bk.ru>
2015-07-04 22:56:58 +0300

    Add JSONConfig class, use it for Config class

--- modules/class/config.php
+++ modules/class/config.php
@@ -4,43 +4,75 @@
 Config::init();
 
 
-class Config
+class JSONConfig
 {
-	const CONFIG_FILE = "config/config.json";
-	static private $config;
-	
+	const FILE = "";
+	const REQUIRED = false;
+	const PRETTY = false;
+	const NOTY = true;
+
+	static private $config = array();
+
 	static public function init()
 	{
-		if( !file_exists(self::CONFIG_FILE) )
-			throw new Exception( "Config file ". self::CONFIG_FILE ." not found" );
+		$class = get_called_class();
 
-		self::$config = json( file_get_contents(self::CONFIG_FILE) );
-		if( self::$config === null )
-			throw new Exception( "Config file parse error" );
+		if( !file_exists(static::FILE) )
+		{
+			if( static::REQUIRED )
+				throw new Exception( __METHOD__.": file ". self::FILE ." is not found" );
+
+			self::$config[$class] = array();
+			return;
+		}
+
+		self::$config[$class] = json( file_get_contents(static::FILE) );
+		if( self::$config[$class] === null )
+			throw new Exception( __METHOD__.": config file parse error" );
 	}
-	
-	static public function save( $noty = true )
-	{
-		$res = file_put_contents( self::CONFIG_FILE, json(self::$config, 1) );
-		if( !$res )
-			throw new Exception( "Can't save to ". self::CONFIG_FILE );
 
-		if( $noty )
+	static public function save()
+	{
+		$res = file_put_contents( static::FILE, json(self::$config[get_called_class()], static::PRETTY) );
+		if( !$res )
+			throw new Exception( __METHOD__.": can't save config to ". static::FILE );
+
+		if( static::NOTY )
 			Noty::success( "Настройки сохранены", 2 );
 	}
-	
+
 	static public function get( $key, $subkey=false )
 	{
-		if( !$subkey && isset(self::$config[$key]) )
-			return self::$config[$key];
-		elseif( $subkey && isset(self::$config[$key][$subkey]) )
-			return self::$config[$key][$subkey];
+		$class = get_called_class();
+
+		if( !$subkey && isset(self::$config[$class][$key]) )
+			return self::$config[$class][$key];
+		elseif( $subkey && isset(self::$config[$class][$key][$subkey]) )
+			return self::$config[$class][$key][$subkey];
 		else
 			return false;
 	}
-	
+
+	static public function get_all()
+	{
+		return self::$config[get_called_class()];
+	}
+
 	static public function set( $key, $value )
 	{
-		self::$config[$key] = $value;
+		self::$config[get_called_class()][$key] = $value;
 	}
+
+	static public function set_all( $value )
+	{
+		self::$config[get_called_class()] = $value;
+	}
+}
+
+
+class Config extends JSONConfig
+{
+	const FILE = "config/config.json";
+	const REQUIRED = true;
+	const PRETTY = true;
 }
